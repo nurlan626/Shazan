c6c3e4b699d3ddb926151f365d2ed3e94122efd4
zabrat <zaur1996@bk.ru>
2020-12-09 20:12:26 +0300

    create collumn

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import View from "./view/view";
+// import Model from "./model/model";
+import Controller from "./controller/controller";
+
+function init() {
+    const view = new View();
+    // const model = new Model();
+    const controller = new Controller(view);
+
+    controller.init();
+}
+
+init();
